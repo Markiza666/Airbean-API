5da3e7802f9d4d6a89624212f2ab31f10febb5cf
emilialindberg <lindbergemilia@outlook.com>
2025-06-16 13:31:04 +0200

    Logic for login user

--- routes/users.js
+++ routes/users.js
@@ -4,5 +4,7 @@ import userController from '../controllers/userController.js';
 const router = express.Router();
 
 router.post('/register', userController.registerUser);
+router.post('/login', userController.loginUser);
+
 
 export default router;
