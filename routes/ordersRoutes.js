6e4e0c5c2b72c3a9531dc9c4eb2a91a1a5f871ca
Markiza666 <3ggjs24erikma@folkuniversitetet.nu>
2025-06-18 12:03:16 +0200

    navigation

--- routes/ordersRoutes.js
+++ routes/ordersRoutes.js
@@ -2,11 +2,16 @@ import express from 'express';
 import orderController from '../controllers/orderController.js';
 import { validateMenuAndPrices } from '../middleware/menuValidationMiddleware.js';
 import { validateNewOrder } from '../middleware/validationMiddleware.js';
+import { createOrder, getOrderStatus, getOrderHistory } from '../controllers/;orderController.js';
+import { authenticateToken } from '../middleware/authMiddleware.js';
 
 router.get('/orders/:orderId/status/', authenticateToken, getOrderStatus);
 
 const router = express.Router();
 
-router.post('/', orderController.createOrder, validateMenuAndPrices, validateNewOrder);
+router.post('/orders', authenticateToken, validateNewOrder, validateMenuAndPrices, createOrder);
+router.get('/orders/:orderId/status', authenticateToken, getOrderStatus);
+router.get('/orders/history', authenticateToken, getOrderHistory);
+
 
 export default router;
